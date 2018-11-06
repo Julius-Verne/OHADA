d74d8c7828db1b38895317c11f057029912d9dcb
Julian Hernández <juliusvernedesign@gmail.com>
2018-11-06 14:26:47 -0500

    First commit

---
+++ scripts/import.R
@@ -0,0 +1,10 @@
+sheet.one.bar <- read_csv(here("input-data","Sheet1 - BarGraph.csv"))
+sheet.one.map <- read_csv(here("input-data","Sheet1 - Map.csv"))
+
+sheet.two.bar <- read_csv(here("input-data","Sheet2 - Bar-Pie.csv"))
+sheet.two.top <- read_csv(here("input-data","Sheet2 - Top.csv"))
+
+sheet.three.line <- read_csv(here("input-data","Sheet3 - Line.csv"))
+
+sheet.four.sab.change <- read_csv(here("input-data","Sheet4 - StartingBussiness_Change.csv"))
+sheet.four.gen.change <- read_csv(here("input-data","Sheet4 - General_Change.csv"))
